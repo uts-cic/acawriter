49bddfa7fdb84aeb4d84fb5e5c63987723eefbd7
radhikamogarkar <radhikavm@telstra.com>
2017-10-10 15:39:27 +1100

    auth for user_roles

--- app/User.php
+++ app/User.php
@@ -31,4 +31,40 @@ class User extends Authenticatable
     public function roles() {
         return $this->belongsToMany('App\Role', 'user_role');
     }
+
+    public function authorizeRoles($roles)
+    {
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'This action is unauthorized.');
+    }
+
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->roles()->where(‘name’, $role)->first()) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
