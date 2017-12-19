eb399d51d6d03bbad5970a7078af1b5f92cad309
EC2 Default User <ec2-user@ip-172-31-12-14.ap-southeast-2.compute.internal>
2017-12-19 22:16:32 +0000

    staging initial commit

--- socket.js
+++ socket.js
@@ -8,8 +8,11 @@ var app     = require('express')();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
 var Redis   = require('ioredis');
+/** non redis 
 var redis   = new Redis();
+**/
 
+var redis = new Redis({host:'redis'});
 
 http.listen(3000,function(){
    console.log('Listening on port 3000');
