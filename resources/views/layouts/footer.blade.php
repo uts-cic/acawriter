3bb186b9bfa6025f020701dd37cfd4f764eb2b2d
radhikamogarkar <radhikavm@telstra.com>
2018-01-12 14:23:01 +1100

    Feedback Toggle UI updates 1 of 2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,25 @@
-<footer class="footer">
-    <div class="bg-dark text-white">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-2">
-                    <a class="text-white">About Us</a>
-                </div>
-                <div class="col-md-2">
-                    <a class="text-white">Contact Us</a>
-                </div>
-            </div>
-            <hr />
-            <div class="row">
-                <div class="col-md-12 mx-auto"><small>© Copyright UTS - CRICOS Provider No: 00099F. The page is authorised by Deputy Vice-Chancellor and Vice-President (Corporate Services).</small></div>
+<footer class="footer bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 h-100 text-center text-lg-left my-auto">
+                <ul class="list-inline mb-2">
+                    <li class="list-inline-item">
+                        <a href="#">About</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Contact</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Terms of Use</a>
+                    </li>
+                    <li class="list-inline-item">&sdot;</li>
+                    <li class="list-inline-item">
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                </ul>
+                <p class="small mb-4 mb-lg-0">&copy; Copyright UTS - CRICOS Provider No: 00099F. The page is authorised by Deputy Vice-Chancellor and Vice-President (Corporate Services).</p>
             </div>
         </div>
     </div>
