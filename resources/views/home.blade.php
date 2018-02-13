e5b68dd3faea1b10b63b9e87da4797253b75d8be
radhikamogarkar <radhikavm@telstra.com>
2018-02-13 15:41:19 +1100

    default feature for analytic & update genre dropbox in assignments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -175,7 +175,43 @@
                 </div>
             </div>
         </div>
-        @endif
+
+
+
+        @elseif(in_array('staff', $data->roles))
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">System Status</div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @if (session('status'))
+                            <li class="list-group-item list-group-item-success">
+                                {{ session('status') }}
+                            </li>
+                            @endif
+                            <li class="list-group-item list-group-item-info" role="alert">You are logged in!</li>
+                            <internet-connection :last-heart-beat-received-at="lastHeartBeatReceivedAt"></internet-connection>
+                            <tap-status :tap-health="tapHealth"></tap-status>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header bg-dark text-white">Admin</div>
+                    <div class="card-body">
+                        <a href="/assignment" class="list-group-item list-group-item-action"><i class="fa fa-clone"></i>  Manage Assignments</a>
+                    </div>
+                </div>
+            </div>
+            @endif
+
+
+
 
     </div>
 
