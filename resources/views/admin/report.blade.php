2be2469b767b4a2e817e953df13c0e26fb5a7115
chezecz <cheze92@gmail.com>
2020-03-05 17:33:52 +1100

    add search documents form

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -77,6 +77,29 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
+                            <div class="card-header">Search Documents</div>
+                            <div class="card-body">
+                                <form action="/admin/diffDocuments" method="post">
+                                    <div class="row">
+                                        <div class="form-group col-sm-12 col-md-3">
+                                            <input type="text"  class="form-control" name="document_id" placeholder="enter document id" />
+                                        </div>
+                                        <div class="form-group col-sm-12 col-md-9">
+                                            <div class="btn-group" role="group" aria-label="Basic example">
+                                                <button href="#" class="btn btn-primary" type="submit" name="action" value="show"><i class="fa fa-search" aria-hidden="true"></i>Documents</button>
+                                            </div>
+                                        </div>
+
+                                        {{ csrf_field() }}
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
                             <div class="card-header">Results</div>
                             <table class="table">
                                 <thead class="thead-dark">
