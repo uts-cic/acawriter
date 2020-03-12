5c66615e3e1edc29cf229fb116c96a594c300640
chezecz <cheze92@gmail.com>
2020-03-12 11:58:01 +1100

    send object to compare

--- resources/views/admin/diffreport.blade.php
+++ resources/views/admin/diffreport.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="app" class="container-fluid">
     @if (isset($data->draft_first))
-    <diff-report document="{{json_encode($data->draft_first)}}" :user-activity="userActivity"></diff-report>
+    <diff-report document="{{json_encode($data->draft_first)}}" document_compare="{{json_encode($data->draft_second)}}" :user-activity="userActivity"></diff-report>
     @else
     <diff-report document=""></diff-report>
     @endif
