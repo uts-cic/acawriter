91d7da5a9b1b45437af59e68608a6eb31b453a04
Andrey Inkin <andrey.inkin@uts.edu.au>
2020-05-08 11:40:51 +1000

    Login page updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,10 @@
     @endif
 
     <h1>Login</h1>
-    <p class="lead">Non UTS and other authorised users only</div>
+    <p class="lead">AcaWriter is available to all UTS staff and students.</p>
+    <p class="mb-5"><a class="btn btn-primary" href="{{ env('AAF_LINK', '') }}">UTS Staff and Students Login</a></p>
+
+    <h2>Authorised non-UTS users only</h2>
     <div>
         @include('auth.form')
     </div>
