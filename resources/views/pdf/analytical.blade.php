d1d8ad0e687affc39f910695237bb30ee1cbf717
Andrey Inkin <Andrey.Inkin@uts.edu.au>
2024-06-27 12:26:49 +1000

    Polyfill security fix

--- resources/views/pdf/analytical.blade.php
+++ resources/views/pdf/analytical.blade.php
@@ -173,7 +173,6 @@
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js"></script>
     <script src="/js/app.js"></script>
 </body>
 </html>
